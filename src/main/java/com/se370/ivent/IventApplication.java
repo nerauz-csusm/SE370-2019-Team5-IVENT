8c03f49dbd493a3322824130f206a2a3009ce081
liam <liam.thuaire@epitech.eu>
2020-02-20 14:13:44 -0800

    base repot

---
+++ src/main/java/com/se370/ivent/IventApplication.java
@@ -0,0 +1,13 @@
+package com.se370.ivent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se370/ivent/IventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se370.ivent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
