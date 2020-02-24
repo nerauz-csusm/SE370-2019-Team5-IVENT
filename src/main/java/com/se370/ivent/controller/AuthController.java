f8e46427032c1882ff2a3daf639fc53f8c6d24ec
liam <liam.thuaire@epitech.eu>
2020-02-24 15:38:38 -0800

    register + login template

---
+++ src/main/java/com/se370/ivent/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.se370.ivent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+    @RequestMapping("/login")
+    public String signinPage() {
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+}
